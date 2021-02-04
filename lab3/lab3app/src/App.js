5b11598b51a37f47d34cc36306c61c1614995a2a
sskurnick <sskurnick@knights.ucf.edu>
2021-02-04 17:12:32 -0500

    lab3

---
+++ lab3/lab3app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomePage from './homepage'
+
+function App() {
+  const fname = 'Sam';
+  return (
+    <HomePage firstName={fname} />
+  )
+}
+
+export default App;
